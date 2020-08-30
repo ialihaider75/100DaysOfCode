2b146e9a2cbbf24fe86c55be55838a4e99f976be
Alihaider122 <a.h459313@gmail.com>
2020-08-30 20:12:11 +0500

    Adding more practiced excercises of javascript

--- Day6/js/app.js
+++ Day6/js/app.js
@@ -144,3 +144,18 @@ function myBooleanFunction() {
 }
 
 console.log("my boolean value is: " + myBooleanFunction());
+
+// if condition in javascript
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+
+    // Only change code above this line
+
+}
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
