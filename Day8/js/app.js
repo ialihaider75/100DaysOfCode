dbde130e46f9e9ff64be5851985b4511893e5518
Alihaider122 <a.h459313@gmail.com>
2020-09-01 16:46:07 +0500

    giving final touch

--- Day8/js/app.js
+++ Day8/js/app.js
@@ -12,10 +12,14 @@ function addTask() {
     domTaskElementRemoveBtn.innerHTML = "<i class='fas fa-window-close'></i>";
     domTaskElementRemoveBtn.addEventListener("click", function () {
         removeTask(this.parentNode.getAttribute("id"));
-    })
+    });
     domTaskElement.append(document.createElement("span").innerText = taskObject.task);
     domTaskElement.append(domTaskElementRemoveBtn);
+    domTaskElement.addEventListener("click", function () {
+        markTaskAsDone(this);
+    });
     document.querySelector(".task-list").append(domTaskElement);
+    document.querySelector(".task-count").innerText = document.getElementsByClassName("task").length;
     taskInput.value = "";
 }
 
@@ -28,4 +32,9 @@ function generateUniqueIdForNewTask() {
 function removeTask(id) {
     task = document.getElementById(id);
     task.parentNode.removeChild(task);
+    document.querySelector(".task-count").innerText = document.getElementsByClassName("task").length;
+}
+
+function markTaskAsDone(task) {
+    task.style.backgroundColor = "#29db2f";
 }
