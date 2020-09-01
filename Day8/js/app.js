6ac10883661d41d1763d683ea458fe1675236112
Alihaider122 <a.h459313@gmail.com>
2020-09-01 15:33:22 +0500

    Finishing up toDo list

--- Day8/js/app.js
+++ Day8/js/app.js
@@ -6,8 +6,13 @@ function addTask() {
     };
     var domTaskElement = document.createElement("div");
     domTaskElement.setAttribute("id", taskObject.id);
+    domTaskElement.setAttribute("class", "task");
     var domTaskElementRemoveBtn = document.createElement("a");
+    domTaskElementRemoveBtn.setAttribute("class", "task-remove-btn");
     domTaskElementRemoveBtn.innerHTML = "<i class='fas fa-window-close'></i>";
+    domTaskElementRemoveBtn.addEventListener("click", function () {
+        removeTask(this.parentNode.getAttribute("id"));
+    })
     domTaskElement.append(document.createElement("span").innerText = taskObject.task);
     domTaskElement.append(domTaskElementRemoveBtn);
     document.querySelector(".task-list").append(domTaskElement);
@@ -16,7 +21,11 @@ function addTask() {
 
 function generateUniqueIdForNewTask() {
     var taskList = document.querySelector(".task-list");
-    console.log(taskList);
     var index = taskList.getElementsByTagName("div").length;
     return index;
+}
+
+function removeTask(id) {
+    task = document.getElementById(id);
+    task.parentNode.removeChild(task);
 }
